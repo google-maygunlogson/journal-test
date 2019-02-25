25b027a2eefd0811f46b64a991e01ad8e1e4d878
Sheldon May-gunlogson <maygunlogson@maygunlogson-macbookpro.roam.corp.google.com>
2019-02-25 15:41:00 -0800

    Updates

---
+++ precache-manifest.c0b1ec3ea746548b1a35487f24305c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52ee12b698be286e95f5",
+    "url": "/journal-test/static/css/main.3482494c.chunk.css"
+  },
+  {
+    "revision": "52ee12b698be286e95f5",
+    "url": "/journal-test/static/js/main.52ee12b6.chunk.js"
+  },
+  {
+    "revision": "42f7d5dc0c5f8eb074fe",
+    "url": "/journal-test/static/js/runtime~main.42f7d5dc.js"
+  },
+  {
+    "revision": "4596feb6d4a00ffa8757",
+    "url": "/journal-test/static/js/2.4596feb6.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/journal-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9349160c3f7a71b1d88e5e7b9f4a1a5a",
+    "url": "/journal-test/index.html"
+  }
+];
